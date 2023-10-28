c7919e01680e37d94b4b58918106a2e280ab7d59
Adriano Vitor <advg1907@gmail.com>
2023-10-28 16:34:36 -0300

    Update Properties Validation

--- ManageLibrary_Domain/Entities/Customer.cs
+++ ManageLibrary_Domain/Entities/Customer.cs
@@ -19,8 +19,7 @@ namespace ManageLibrary_Domain.Entities {
             DomainExceptionValidation.When(name == null || name.Length > 100, "O nome não pode ser vazio e nem ter mais do que 100 caracteres");
             DomainExceptionValidation.When(cpf == null || cpf.Length > 11, "Digite um CPF válido");
             DomainExceptionValidation.When(phone.Length > 11, "Digite um telefone válido");
-            DomainExceptionValidation.When(email.Length > 100, "O email não pode ser vazio e nem ter mais do que 100 caracteres");
-            DomainExceptionValidation.When(email == null && phone == null, "Por favor informe o Email ou o Telefone para contato");  
+            DomainExceptionValidation.When(email.Length > 100, "O email não pode ter mais do que 100 caracteres");  
 
             Name = name;
             CPF = cpf;
